8df0e470c8b0b27d2db9f0b3460965a36f0c95cb
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2024-01-11 09:10:08 -0300

    Aulas e fim

--- src/views/CursoView.vue
+++ src/views/CursoView.vue
@@ -5,14 +5,19 @@
     </div>
     <transition>
       <div v-if="api" class="conteudo">
-        <h1>{{ api.nome }}</h1>
-        <p>{{ api.descricao }}</p>
-        <h2>Aulas</h2>
-        <ul class="aulas">
-          <li v-for="aula in api.aulas" :key="aula.id">
-            {{ aula.nome }}
-          </li>
-        </ul>
+        <div>
+          <h1>{{ api.nome }}</h1>
+          <p>{{ api.descricao }}</p>
+          <h2>Aulas</h2>
+          <ul class="aulas">
+            <li v-for="aula in api.aulas" :key="aula.id">
+              <router-link :to="{ name: 'aula', params: { aula: aula.id } }">{{
+                aula.nome
+              }}</router-link>
+            </li>
+          </ul>
+        </div>
+        <router-view></router-view>
       </div>
     </transition>
   </div>
@@ -31,12 +36,17 @@ export default {
 </script>
 
 <style>
-.aulas li {
+.aulas li a {
   display: block;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
   background: white;
   padding: 20px;
   margin-bottom: 10px;
   border-radius: 4px;
+}
+
+.aulas li a.router-link-active {
+  background: #4b8;
+  color: white;
 }
 </style>
